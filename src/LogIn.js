3b6ce6a90e67e935f8999dc32b6d781f898118ae
erica2605@live.com <erica2605@live.com>
2020-07-10 13:34:14 -0400

    log in component created

---
+++ src/LogIn.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./css/style.css";
+
+const LogIn = () => (
+  <div>
+    <li className="dropdown">
+      <Link
+        to=""
+        className="nav-link dropdown-toggle"
+        role="button"
+        data-toggle="dropdown"
+      >
+        Log In
+      </Link>
+      <ul className="dropdown-menu">
+        <form>
+          <label>
+            <li>
+              <Link to="">
+                <input type="text" name="email" placeholder="email" />
+              </Link>
+            </li>
+            <li>
+              <Link to="">
+                <input type="password" name="password" placeholder="password" />
+              </Link>
+            </li>
+          </label>
+          <li>
+            <Link to="">
+              <input className="login-submit" type="submit" value="Log In" />
+            </Link>
+          </li>
+        </form>
+      </ul>
+    </li>
+  </div>
+);
+
+export default LogIn;
