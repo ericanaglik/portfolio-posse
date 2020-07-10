dd7457027dfb89f69160e1a0f2078b1fa7b3c8db
erica2605@live.com <erica2605@live.com>
2020-07-10 13:36:50 -0400

    login component inside landing

--- src/Landing.js
+++ src/Landing.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./css/style.css";
+import LogIn from "./LogIn"
 
 const Landing = () => (
   <div>
@@ -38,45 +39,7 @@ const Landing = () => (
                 Example Templates
               </Link>
             </li>
-            <li className="dropdown">
-              <Link
-                to=""
-                className="nav-link dropdown-toggle"
-                role="button"
-                data-toggle="dropdown"
-              >
-                Log In
-              </Link>
-              <ul className="dropdown-menu">
-                <form>
-                  <label>
-                    <li>
-                      <Link to="">
-                        <input type="text" name="email" placeholder="email" />
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <input
-                          type="password"
-                          name="password"
-                          placeholder="password"
-                        />
-                      </Link>
-                    </li>
-                  </label>
-                  <li>
-                    <Link to="">
-                      <input
-                        className="login-submit"
-                        type="submit"
-                        value="Log In"
-                      />
-                    </Link>
-                  </li>
-                </form>
-              </ul>
-            </li>
+            <LogIn/>
           </ul>
         </div>
       </nav>
