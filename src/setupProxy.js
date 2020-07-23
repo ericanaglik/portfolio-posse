d9b126ce61cae51f59d99698380d12befe02f908
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-07-23 14:11:38 -0700

    change proxy info to relevant info

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,11 @@
-// const proxy = require("http-proxy-middleware");
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware("/auth", { target: "http://localhost:5000/" }));
+    app.use(createProxyMiddleware("/auth", { 
+        target: "http://localhost:5000/",
+        // headers: {
+        //     "Connection": "keep-alive"
+        // }
+    }));
 };
 
