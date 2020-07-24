68f510485d79ea8235c44d7644698889379e2801
erica2605@live.com <erica2605@live.com>
2020-07-23 23:48:26 -0400

    updated app.js to include routes from signup.jsx

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Landing from './Landing'
-import Signup from './Signup'
+import history from "./history";
+import UserProvider from "./contexts/UserProvider";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import "./style/index.css"
 
 const App = () => (
-  <Router>
-    <div>
-      <Route path="/" exact component={Landing} />
-      <Route path="/signup" exact component={Signup}/>
-     </div>
+  <div>
+    <Router history={history}>
+    <UserProvider>
+        <Route path="/profile" component={Profile} />
+    </UserProvider>
+    <Route path="/" exact component={Landing} />
+    <Route path="/signup" exact component={Home} />
   </Router>
+  </div>
   );
 
 export default App;
