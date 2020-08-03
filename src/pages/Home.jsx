f1adf76468092ea2db1182748dd0d2e4175802ff
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-08-03 00:06:35 -0700

    change styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,24 +6,24 @@ import NavBar from "../NavBar"
 const Home = () => {
     return (
       <div>
-        
-      
         <div className="page" style={{ textAlign: "center" }}>
         <div className="nav-container">
-        <header className="auth_header_area">
-      <NavBar/>
-    </header>
-    </div>
+          <header className="auth_header_area">
+            <NavBar/>
+          </header>
+        </div>
         <div className="container-auth">
+          
         <div className="progress-bar">
           <StepProgressBar/>
-          </div>
-            <p className="page-title">Sign Up</p>
-            <p style={{ fontSize: 20, padding: 30, paddingBottom: 60, paddingTop: 60}}>
-                Create your portfolio today. Portolio Posse has easy and fast sign up using your account with any of these apps.
-            </p>
-            <CardList />
-            </div>
+        </div>
+        
+        <p className="page-title">Sign Up</p>
+        <p style={{ fontSize: 20, padding: 30, paddingBottom: 60, paddingTop: 60}}>
+            Create your portfolio today. <br /> Portfolio Posse has easy and fast sign up <br />using your account with any of these apps.
+        </p>
+        <CardList />
+        </div>
         </div>
         <div className="background-gradient"></div>
         </div>
