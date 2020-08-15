f80e6cb03ce4884372ae5c02baaa1cb6a21bc812
erica2605@live.com <erica2605@live.com>
2020-08-15 14:54:08 -0400

    removed unused imports

--- src/data/index.js
+++ src/data/index.js
@@ -2,9 +2,7 @@ import AmazonIcon from "../res/amazon.png";
 import FacebookIcon from "../res/facebook.png";
 import GithubIcon from "../res/github.png";
 import GoogleIcon from "../res/google.png";
-import InstagramIcon from "../res/instagram.png";
-import SpotifyIcon from "../res/spotify.png";
-import TwitchIcon from "../res/twitch.png";
+
 export const data = [{
     img: AmazonIcon,
     name: "amazon",
